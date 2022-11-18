8036ac3ce6184542272f4dc0dded2717cc8a7382
spderz12 <116905268+spderz12@users.noreply.github.com>
2022-11-18 10:04:30 -0500

    Create main class and blackjack class

---
+++ src/Games/Blackjack.java
@@ -0,0 +1,82 @@
+package Games;
+
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Blackjack {
+    public static final Scanner scan = new Scanner (System.in);
+    public static void play()
+    {
+       boolean running = true;
+       int altAce = 0;
+        System.out.println("<<Blackjack>>");
+        System.out.println("What amount will you wager? (Minimum $25)"); //used for winnings later
+        while (true) {
+            int wager = scan.nextInt();
+            if (wager < 25)
+                System.out.println("Not enough cash, try again!");
+            else {
+                //TODO Subtract bet from user balance
+                int runningTotal = 0;//check to see if 21 is reached
+                int dealerTotal = 0;//used later for opponent
+                while (running) {
+
+                    String card = null;
+                    for (int i = 0; i < 2; i++) {
+                        card = getCard();//automatically draws two cards at beginning of game
+                        if (card.equals("Ace")) {
+                            altAce += 1;//alternate ace for multiple value task
+                        }
+                    }
+
+                    System.out.println("You drew: " + card);
+
+                    HashMap<String, Integer> cardValues = new HashMap<>();
+                    cardValues.put("Jack", 10);
+                    cardValues.put("Queen", 10);
+                    cardValues.put("King", 10);
+                    cardValues.put("Ace", 11);
+                    //assigning hashmap to change card names to number values
+                    if (cardValues.containsKey(card)) {
+                        runningTotal += cardValues.get(card); //To avoid error, checks if card is in hashmap
+                    }
+                    else {
+                        int cardInt = Integer.parseInt(card);
+                        runningTotal += cardInt;
+                    }
+                    System.out.println("total = " + runningTotal);
+                   //TODO show current card values and also alternative if ace present (altAce >= 1)
+                    //TODO make ace worth 1 or 11 in certain contexts (use if condition "or")
+                    System.out.println("again?");
+                    String ans = scan.nextLine();
+                    if (ans.equals("no"))
+                    break;
+                }
+
+            }
+        }
+    }
+
+    static String getCard(){
+        String card;
+
+        int tCard = (int)(13*Math.random()+2);
+
+        card = Integer.toString(tCard);
+
+        if (card.contains("11")){
+            card = card.replace("11", "Jack");
+        }
+        else if ( card.contains("12")){
+            card = card.replace("12", "Queen");
+        }
+        else if (card.contains("13")){
+            card = card.replace("13", "King");
+        }
+        else if (card.contains("14")) {
+            card = card.replace("14", "Ace");
+        }
+
+        return card;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.util.Scanner;
+import Games.*;
+
+public class Main {
+    public static final Scanner scan = new Scanner (System.in);
+    public static void main(String[] args) {
+        System.out.println("<<CASINO>>");
+        System.out.println("What would you like to do?\n(For a command list, type help)");
+        while (true){
+            String ans = scan.nextLine();
+
+            if (ans.equalsIgnoreCase("blackjack")){
+                //blackjack class
+                Blackjack.play();
+            }
+            else if (ans.equalsIgnoreCase("slots")){
+                //slots class
+            }
+            else if (ans.equalsIgnoreCase("lotto")){
+                //roulette class
+            }
+            else if (ans.equalsIgnoreCase("poker")){
+                //make player vs house poker game with one swap with dealer mechanic
+            }
+            else if (ans.equalsIgnoreCase("wallet")){
+                //checks chip balance and can grant loans
+            }
+            else if (ans.equalsIgnoreCase("exit")){
+                System.out.println("\nClosing Program");
+                System.exit(0);
+            }
+            else if (ans.equalsIgnoreCase("help")){
+                System.out.println("COMMANDS:\nBlackjack - play a game of blackjack\nSlots - play a slot machine");
+                System.out.println("Lotto - play the lotto\nPoker - play a modified poker game\nWallet - check balance and get loans\n Exit - exit program");
+            }
+            else
+                System.out.println("Undefined command");
+
+        }
+    }
+}
